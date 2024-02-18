81cbca507a3df620d8e27e8f87ffd770f0947c67
saujan1788 <saujan1788@gmail.com>
2024-02-18 18:44:59 +0000

    Ansible Works

--- Terraform/modules/k8s_infra/main.tf
+++ Terraform/modules/k8s_infra/main.tf
@@ -1,12 +1,15 @@
 resource "aws_instance" "k8s_Instance" {
   count         = var.instance_count
-  ami           = "ami-0766b4b472db7e3b9"
+  ami           = "ami-0905a3c97561e0b69"
   instance_type = "t2.medium"
   subnet_id     = "subnet-94311af2"  # Use the same subnet for all instances
+  key_name      = "K8s_control"
 
   vpc_security_group_ids = ["sg-0cc759ffd6251c8ff"]
 
   tags = {
-    Name = "k8s_Instance-${count.index}"
+    Name = count.index == 0 ? "master" : "worker-${count.index}"
   }
 }
+
+
