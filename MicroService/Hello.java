edefe959bd2574fac5dcc0840d9a21bc4a04e9dc
saujan1788 <saujan1788@gmail.com>
2024-02-22 14:38:34 +0000

    Java_loop

--- MicroService/Hello.java
+++ MicroService/Hello.java
@@ -2,6 +2,17 @@ package MicroService;
 
 public class Hello{
     public static void main(String[] args) {
-        System.out.println("Hello World, Saujan is trying containers");
+	while (true) {
+	
+       System.out.println("Hello World, Saujan is trying containers");
+	
+
+	try {
+		Thread.sleep(10000);
+
+	} catch (InterruptedException e){
+		e.printStackTrace();
+	   }
+	}
     }
 }
