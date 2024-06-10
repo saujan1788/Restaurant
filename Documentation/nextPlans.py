8dcc8dc073a24ba8330ec9f8e87cefe04d6906f1
saujan1788 <saujan1788@gmail.com>
2024-06-10 09:14:16 +0000

    10thTaskCompleted

--- Documentation/nextPlans.py
+++ Documentation/nextPlans.py
@@ -1,2 +1,9 @@
 def EnableMonitoring:
     To learn Grafana and prometheus , I want to monitor the cluster , Look into into it , Date: 25/05/2024
+
+def K8sClusterMonitoring:
+    Spend some time on monitoring the k8s cluster. Work a new pipeline for a microservice application., Date 10/06/2024
+
+
+def Pipelines:
+    Try to get more creative on this. Add different types of pipelines., Date 10/06/2024
